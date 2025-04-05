5d11e74740988f013ca57d8c94b05046c9a83d2a
jettmanalo <jettmanalo.dev@gmail.com>
2025-04-05 15:00:00 +0800

    Added AppContext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { AppContextProvider } from "./context/AppContext.jsx";
 
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowserRouter>
+);
