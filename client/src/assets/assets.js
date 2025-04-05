f5195ba8b008b8a8365a544ddaf7b61c0a7e6728
jettmanalo <jettmanalo.dev@gmail.com>
2025-04-05 15:49:52 +0800

    Created Navbar component

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,3 +1,4 @@
+import favicon from "./favicon.svg";
 import search_icon from "./search_icon.svg";
 import remove_icon from "./remove_icon.svg";
 import arrow_right_icon_colored from "./arrow_right_icon_colored.svg";
@@ -70,6 +71,7 @@ import brown_rice_image from "./brown_rice_image.png";
 import barley_image from "./barley_image.png";
 
 export const assets = {
+  favicon,
   search_icon,
   remove_icon,
   arrow_right_icon_colored,
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useAppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
+
+const Navbar = () => {
+  const [open, setOpen] = React.useState(false);
+  const { user, setUser, setShowUserLogin, navigate } = useAppContext();
+
+  const logout = async () => {
+    setUser(null);
+    navigate("/");
+  };
+
+  return (
+    <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
+      <NavLink
+        className="flex items-center"
+        to="/"
+        onClick={() => setOpen(false)}
+      >
+        <img
+          className="h-9 me-2"
+          src={assets.favicon}
+          alt="FreshlyPicked logo"
+        />
+        <p className="font-bold">FreshlyPicked</p>
+      </NavLink>
+
+      {/* Desktop Menu */}
+      <div className="hidden sm:flex items-center gap-8">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/products">Producs</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+
+        <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
+          <input
+            className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
+            type="text"
+            placeholder="Search products"
+          />
+          <img src={assets.search_icon} alt="search" className="w-4 h-4" />
+        </div>
+
+        <div
+          onClick={() => {
+            navigate("/cart");
+          }}
+          className="relative cursor-pointer"
+        >
+          <img src={assets.cart_icon} alt="cart" className="w-6 opacity-80" />
+          <button className="absolute -top-2 -right-3 text-xs text-white bg-green w-[18px] h-[18px] rounded-full">
+            3
+          </button>
+        </div>
+
+        {!user ? (
+          <button
+            onClick={() => setShowUserLogin(true)}
+            className="cursor-pointer px-8 py-2 bg-green hover:bg-greener transition text-white rounded-full"
+          >
+            Login
+          </button>
+        ) : (
+          <div className="relative group">
+            <img src={assets.profile_icon} className="w-10" alt="" />
+            <ul className="hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40">
+              <li
+                onClick={() => navigate("my-orders")}
+                className="p-1.5 pl-3 hover:bg-green/10 cursor-pointer"
+              >
+                My Orders
+              </li>
+              <li
+                onClick={logout}
+                className="p-1.5 pl-3 hover:bg-green/10 cursor-pointer"
+              >
+                Logout
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+
+      <button
+        onClick={() => (open ? setOpen(false) : setOpen(true))}
+        aria-label="Menu"
+        className="sm:hidden"
+      >
+        {/* Menu Icon SVG */}
+        <img src={assets.menu_icon} alt="menu" />
+      </button>
+
+      {/* Mobile Menu */}
+      <div
+        className={`${
+          open ? "flex" : "hidden"
+        } absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}
+      >
+        <NavLink to="/" onClick={() => setOpen(false)}>
+          Home
+        </NavLink>
+
+        <NavLink to="/products" onClick={() => setOpen(false)}>
+          Products
+        </NavLink>
+
+        {user && (
+          <NavLink to="/products" onClick={() => setOpen(false)}>
+            My Orders
+          </NavLink>
+        )}
+
+        <NavLink
+          to="/"
+          onClick={() => {
+            setOpen(false);
+          }}
+        >
+          Contact
+        </NavLink>
+
+        {!user ? (
+          <button
+            onClick={() => {
+              setOpen(false);
+              setShowUserLogin(true);
+            }}
+            className="cursor-pointer px-6 py-2 mt-2 bg-green hover:bg-greener transition text-white rounded-full text-sm"
+          >
+            Login
+          </button>
+        ) : (
+          <button
+            onClick={logout}
+            className="cursor-pointer px-6 py-2 mt-2 bg-green hover:bg-greener transition text-white rounded-full text-sm"
+          >
+            Logout
+          </button>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
