c870c1baf47c17b781d895ab21ab17e07bbf127e
jettmanalo <jettmanalo.dev@gmail.com>
2025-04-05 16:14:33 +0800

    Created Mainbanner component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 const App = () => {
+  const isSellerPath = useLocation().pathname.includes("seller");
+
   return (
     <div>
-      <Navbar />
-    </div>
-  )
-}
+      {isSellerPath ? null : <Navbar />}
 
-export default App
+      <div
+        className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
+      >
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
