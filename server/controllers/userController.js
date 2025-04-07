1083bffad3e2cdd575a228a78aad040ffd4da84f
jettmanalo <jettmanalo.dev@gmail.com>
2025-04-07 22:32:56 +0800

    Created login api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import bcrypt from "bcrypt.js";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 // Register User : /api/user/register
@@ -11,7 +11,7 @@ export const register = async (req, res) => {
       return res.json({ success: false, message: "Missing Details" });
     }
 
-    const existingUser = await User.find({ email });
+    const existingUser = await User.findOne({ email });
 
     if (existingUser)
       return res.json({ success: false, message: "User already exists" });
@@ -29,6 +29,49 @@ export const register = async (req, res) => {
       secure: process.env.NODE_ENV === "production", // Use secure cookies in production
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // CSRF protection
       maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expiration time
+    });
+
+    return res.json({
+      success: true,
+      user: { email: user.email, name: user.name },
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Login User : /api/user/login
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res.json({
+        success: false,
+        message: "Email and password are required",
+      });
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "Invalid email or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch)
+      return res.json({ success: false, message: "Invalid email or password" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     return res.json({
