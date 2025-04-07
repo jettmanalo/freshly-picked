fec4d3a51a95c4202459483011abea0a671bacca
jettmanalo <jettmanalo.dev@gmail.com>
2025-04-07 17:10:45 +0800

    Created AddProduct component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,9 @@ import AddAddress from "./pages/AddAddress";
 import MyOrders from "./pages/MyOrders";
 import SellerLogin from "./components/seller/SellerLogin";
 import SellerLayout from "./pages/Seller/SellerLayout";
+import AddProduct from "./components/seller/AddProduct";
+import ProductList from "./components/seller/ProductList";
+import Orders from "./components/seller/Orders";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -48,7 +51,11 @@ const App = () => {
           <Route
             path="/seller"
             element={isSeller ? <SellerLayout /> : <SellerLogin />}
-          ></Route>
+          >
+            <Route index element={isSeller ? <AddProduct /> : null} />
+            <Route path="product-list" element={<ProductList />} />
+            <Route path="orders" element={<Orders />} />
+          </Route>
         </Routes>
       </div>
 
