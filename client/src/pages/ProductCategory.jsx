c07f2c998d9a7fa83b38d39ac472ee43555f0e76
jettmanalo <jettmanalo.dev@gmail.com>
2025-04-14 20:29:37 +0800

    Fixed the issue on category (type array to string)

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -13,7 +13,7 @@ const ProductCategory = () => {
   );
 
   const filteredProducts = products.filter(
-    (product) => product.category.toLowerCase() === category
+    (product) => product.category && product.category.toLowerCase() === category
   );
 
   return (
